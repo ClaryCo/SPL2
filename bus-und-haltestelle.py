a4b69b0af5b4f183efd8531dc76398735e834930
ClaryCo <171wita01@o365.htl-leoben.at>
2018-04-09 15:28:37 +0200

    Änderung

--- bus-und-haltestelle.py
+++ bus-und-haltestelle.py
@@ -4,15 +4,15 @@ i = input ("Wie viele Haltestellen gibt es? : ")
 
 for c in range(0, int(i)) :
 
+
     a = input ("Wie viele Personen steigen ein?: ")
     b = input ("Wie viele Personen steigen aus?: ")
 
     p = p + int(a)
     p = p - int(b)
 
-print (p)
+    print ( p , "Personen sind im Bus")
 
-    if ( p > 60) :
-        print (" Es dürfen nicht mehr Menschen einsteigen ")
-        y = 60-p
-    else:
+    if(p>60):
+        print (" Zuviele im Bus")
+        p = p - int (a)
