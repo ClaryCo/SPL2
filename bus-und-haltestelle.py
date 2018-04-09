bd16244267fe6f2e7e8f717bc481e19bd80d1a2d
ClaryCo <171wita01@o365.htl-leoben.at>
2018-04-09 14:34:48 +0200

    Rechnen

--- bus-und-haltestelle.py
+++ bus-und-haltestelle.py
@@ -1,1 +1,9 @@
 #bus-und-haltestelle
+
+f = eingabe ("Wie viele Personen sind im Bus?")
+i = input ("Bitte geben Sie die Anzahl der Personen der Haltestelle ein")
+
+a = int(eingabe)
+b = int (input)
+
+summe = a + b
